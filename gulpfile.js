258d636d993fee019fe80443131009a9c27dc5bf
Tosho <toso.trajanov@gmail.com>
2015-12-24 12:06:30 +0100

    Add modules

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+/*!
+ * gulp
+ * $ npm install gulp-ruby-sass gulp-autoprefixer gulp-minify-css gulp-jshint gulp-concat gulp-uglify gulp-imagemin gulp-notify gulp-rename gulp-livereload gulp-cache del --save-dev
+ */
+
+// Load plugins
+var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    livereload = require('gulp-livereload'),
+    del = require('del'),
+    sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('styles', function() {
+  return gulp.src('assets/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(minifycss())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist'));
+});
+
+// Clean
+gulp.task('clean', function() {
+  return del(['dist/styles']);
+});
+
+// Default task
+gulp.task('default', ['clean'], function() {
+  gulp.start('styles');
+});
+
+// Watch
+gulp.task('watch', function() {
+
+  // Watch .css files
+  gulp.watch('src/styles/**/*.scss', ['styles']);
+
+  // Create LiveReload server
+  livereload.listen();
+
+  // Watch any files in dist/, reload on change
+  gulp.watch(['dist/**']).on('change', livereload.changed);
+
+});
